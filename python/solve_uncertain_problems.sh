16c1431760c76529c20f8eff981af6878a4251af
pmateusz <mat.polnik@gmail.com>
2019-12-04 18:00:23 +0000

    Synchronization before finish

--- python/solve_uncertain_problems.sh
+++ python/solve_uncertain_problems.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-SOLVER=/home/pmateusz/dev/quake/cmake-build-debug/quake-det
-PROBLEM_DIR=/home/pmateusz/dev/quake/current_review
+SOLVER_DET=/home/pmateusz/dev/quake/cmake-build-debug/quake-det
+PROBLEM_DIR=/home/pmateusz/dev/quake/current_review/problems
 
 PROBLEMS=($PROBLEM_DIR/week_2019-10-01.json:$PROBLEM_DIR/week_2019-10-02.json:$PROBLEM_DIR/week_2019-10-03.json:$PROBLEM_DIR/week_2019-10-04.json:$PROBLEM_DIR/week_2019-10-05.json \
 $PROBLEM_DIR/week_2019-10-06.json:$PROBLEM_DIR/week_2019-10-07.json:$PROBLEM_DIR/week_2019-10-08.json:$PROBLEM_DIR/week_2019-10-09.json:$PROBLEM_DIR/week_2019-10-10.json \
@@ -10,12 +10,6 @@ $PROBLEM_DIR/week_2019-10-21.json:$PROBLEM_DIR/week_2019-10-22.json:$PROBLEM_DIR
 $PROBLEM_DIR/week_2019-10-26.json:$PROBLEM_DIR/week_2019-10-27.json:$PROBLEM_DIR/week_2019-10-28.json:$PROBLEM_DIR/week_2019-10-29.json:$PROBLEM_DIR/week_2019-10-30.json \
 $PROBLEM_DIR/week_2019-10-26.json:$PROBLEM_DIR/week_2019-10-27.json:$PROBLEM_DIR/week_2019-10-28.json:$PROBLEM_DIR/week_2019-10-29.json:$PROBLEM_DIR/week_2019-10-30.json)
 
-for  problem in "${PROBLEMS[@]}"; do
-echo $problem;
+for ((index=0; index<${#PROBLEMS[@]}; ++index)); do
+    $SOLVER_DET --problems="${PROBLEMS[index]}" --gap-limit=0.05 --interval-step=00:01:00 --output=solution_det_$index.json
 done
-
-#$SOLVER
-#--problems=
-#--gap-limit=0.05
-#--interval-step=00:01:00
-#--output=solution_multiple_steps.json
